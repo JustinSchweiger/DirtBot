cb458a8de26fbbb81cb42c589e88a7da301aca0c
Justin Schweiger <Justin.Schweiger@t-online.de>
2022-09-15 19:41:17 +0200

    added most important command of all the important commands

---
+++ src/commands/patthecombi.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('petthecombi')
+        .setDescription('Cause everyone wants to pet combi ...'),
+    async execute(interaction) {
+        await interaction.reply('<a:petthecombi:1020025249770774631>');
+    },
+};
