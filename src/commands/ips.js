ba89af1f38a8eedfb5d27d3b85dd9e2594ce2e21
Justin Schweiger <Justin.Schweiger@t-online.de>
2022-09-15 18:44:45 +0200

    added /ips command

--- src/commands/ips.js
+++ src/commands/ips.js
@@ -1,107 +1,96 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const { File } = require('../helper/GetFileFromGitlab');
-const fs = require('fs');
+const { Logger, Level } = require('../helper/Logger');
+const { readFileSync, statSync } = require('fs');
 const path = require('path');
-const Logger = require('../helper/Logger');
+const { ServersFile } = require('../helper/ServerFile');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ips')
         .setDescription('Shows the ips of all our servers.'),
     async execute(interaction) {
-        const pixelPath = path.join(__dirname, '..', '..', 'assets', 'pixel-ips.json');
-        const modpackPath = path.join(__dirname, '..', '..', 'assets', 'modpack-ips.json');
-        try {
-            const file = await File.get('pixel-ips.json');
-            const json = JSON.parse(file);
-            if (!fs.existsSync(pixelPath) || !fs.readFileSync(pixelPath).equals(Buffer.from(json))) {
-                fs.writeFileSync(pixelPath, JSON.stringify(json, null, 2));
-            }
-        } catch (err) {
-            interaction.reply('Error fetching Pixelmon IPs!');
-            if (err.type === 'invalid-json') {
-                await Logger.log('Error fetching Pixelmon IPs: Invalid JSON', Logger.level.error);
-            } else {
-                await Logger.log(err, Logger.level.error);
-            }
-            return;
-        }
+        await ServersFile.serve(interaction);
+        const serversPath = path.join(__dirname, '..', '..', 'assets', 'servers.json');
 
-        try {
-            const file = await File.get('modpack-ips.json');
-            const json = JSON.parse(file);
-            if (!fs.existsSync(modpackPath) || !fs.readFileSync(modpackPath).equals(Buffer.from(json))) {
-                fs.writeFileSync(modpackPath, JSON.stringify(json, null, 2));
-            }
-        } catch (err) {
-            interaction.reply('Error fetching Modpack IPs!');
-            if (err.type === 'invalid-json') {
-                await Logger.log('Error fetching Modpack IPs: Invalid JSON', Logger.level.error);
-            } else {
-                await Logger.log(err, Logger.level.error);
-            }
-            return;
-        }
-
-        const pixelIps = JSON.parse(fs.readFileSync(pixelPath).toString());
-        const modpackIps = JSON.parse(fs.readFileSync(pixelPath).toString());
-        const pixelStats = fs.statSync(pixelPath);
-        const modpackStats = fs.statSync(modpackPath);
-        const pixelDate = new Date(pixelStats.mtime);
-        const modpackDate = new Date(modpackStats.mtime);
+        const serversJson = JSON.parse(readFileSync(serversPath).toString());
+        const stats = statSync(serversPath);
+        const date = new Date(stats.mtime);
 
         const pixel = [
             {
                 name: '__Pixelmon Server__',
-                value: pixelIps.map(obj => obj.server).join('\n'),
+                value: serversJson
+                    .filter(server => server['isPixelmon'])
+                    .map(server => server['new'] ? `:new: ${server['server']}` : server['server'])
+                    .join('\n'),
                 inline: true,
             },
             {
-                name: 'Minecraft Version',
-                value: `**${pixelIps.map(obj => obj.version).join('\n')}**`,
+                name: '\u200b',
+                value: '\u200b',
                 inline: true,
             },
             {
-                name: '__IP__',
-                value: pixelIps.map(obj => obj.ip).join('\n'),
+                name: '__Server IPs__',
+                value: serversJson
+                    .filter(server => server['isPixelmon'])
+                    .map(server => server['ip'])
+                    .join('\n'),
                 inline: true,
             },
         ];
 
         const modpack = [
             {
-                name: '__Modpack Server__',
-                value: modpackIps.map(obj => obj.server).join('\n'),
+                name: '__Modpack__',
+                value: serversJson
+                    .filter(server => !server['isPixelmon'])
+                    .map(server => server['new'] ? `:new: ${server['server']}` : server['server'])
+                    .join('\n'),
                 inline: true,
             },
             {
-                name: 'Minecraft Version',
-                value: `**${modpackIps.map(obj => obj.version).join('\n')}**`,
-                inline: true,
-            },
-            {
-                name: '__IP__',
-                value: modpackIps.map(obj => obj.ip).join('\n'),
+                name: '__Server IP__',
+                value: serversJson
+                    .filter(server => !server['isPixelmon'])
+                    .map(server => server['ip'])
+                    .join('\n'),
                 inline: true,
             },
         ];
 
+        let extra;
+        try {
+            extra = JSON.parse(await File.get('extra.json'));
+        } catch (err) {
+            await Logger.log('Error fetching extra.json', Level.ERROR);
+            return;
+        }
+
         const pixelEmbed = new EmbedBuilder()
             .setColor('#df0000')
-            .setTitle('<:redbulletpoint:1019567816757751888> DirtCraft Pixelmon Server IPs <:redbulletpoint:1019567816757751888>')
+            .setTitle(`${extra['bulletpoint']} DirtCraft Pixelmon Server IPs ${extra['bulletpoint']}`)
             .addFields(pixel)
-            .setImage('https://i.imgur.com/GdrLxI7.gif')
-            .setFooter({ text: 'Last Update ', iconURL: 'https://i.imgur.com/eyyy6A4.png' })
-            .setTimestamp(pixelDate);
+            .setImage(extra['pixel-banner'])
+            .setFooter({ text: 'Last Updated ', iconURL: extra['footer-icon'] })
+            .setTimestamp(date);
 
         const modpackEmbed = new EmbedBuilder()
             .setColor('#df0000')
-            .setTitle('<:redbulletpoint:1019567816757751888> DirtCraft Modpack Server IPs <:redbulletpoint:1019567816757751888>')
+            .setTitle(`${extra['bulletpoint']} DirtCraft Modpack Server IPs ${extra['bulletpoint']}`)
             .addFields(modpack)
-            .setImage('https://i.imgur.com/C6houqk.gif')
-            .setFooter({ text: 'Last Update ', iconURL: 'https://i.imgur.com/eyyy6A4.png' })
-            .setTimestamp(modpackDate);
+            .setImage(extra['modpack-banner'])
+            .setFooter({ text: 'Last Updated ', iconURL: extra['footer-icon'] })
+            .setTimestamp(date);
 
-        interaction.reply({ embeds: [pixelEmbed, modpackEmbed] });
+        const hubEmbed = new EmbedBuilder()
+            .setColor('#df0000')
+            .setTitle(`${extra['bulletpoint']} DirtCraft Hub${extra['bulletpoint']}`)
+            .setDescription('You can connect to the hub by using the IP `dirtcraft.gg` or `pixelmon.gg`.\nFrom there you can connect to all of our `1.7.10`, `1.10.2` and `1.12.2` servers!\n \nThere is currently no hub for our `1.16.5` servers!\nPlease use the direct ip for now!')
+            .setFooter({ text: 'Last Updated ', iconURL: extra['footer-icon'] })
+            .setTimestamp(date);
+
+        interaction.reply({ embeds: [pixelEmbed, modpackEmbed, hubEmbed] });
     },
 };
